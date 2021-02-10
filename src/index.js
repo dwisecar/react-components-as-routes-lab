d1e30acff4758d350530b414a8d4ec3aa8d022d6
dwisecar <dwisecar@gmail.com>
2021-02-10 11:09:26 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
+import movies from './data'
 
 render (
-  <App />,
+  <App movies={movies}/>,
   document.getElementById('root')
 );
